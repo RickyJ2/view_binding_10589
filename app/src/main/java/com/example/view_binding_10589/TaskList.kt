c9f31b8b43e5b78f5ea09153f1feec4f4392480f
RickyJ2 <Gemini.rickyjunaidi@gmail.com>
2022-09-15 08:36:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10589/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10589
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hair makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari primo buat Gacha Mama Yaemiko tercinta / Keqing Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei?", "Buat adu mekanik showcase ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buta pamer ke view shocase ayaka Pyro C6 + Bennet C6")
+    )
+}
